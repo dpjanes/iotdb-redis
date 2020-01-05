0e64f6e9335717832c461dc532dfc4be94e4f5a9
David Janes <david@consensas.com>
2020-01-05 00:41:15 -0500

    cleanup extensively

--- test/set.json.js
+++ test/set.json.js
@@ -33,12 +33,38 @@ const redis$cfg = require("./data/redis$cfg.json")
 const _util = require("./_util")
 
 describe("set.json", function() {
+    const KEY = "test/set.json.1"
+
+    it("fail - missing key", function(done) {
+        _.promise()
+            .then(_util.initialize)
+        
+            .add({
+                "key": null,
+                "value": "a value",
+            })
+            .then(redis.set.json)
+            .then(_util.auto_fail(done))
+            .catch(_util.ok_error(done))
+    })
+    it("fail - json is not-JSON", function(done) {
+        _.promise()
+            .then(_util.initialize)
+        
+            .add({
+                "key": "test/set.1",
+                "value": () => {},
+            })
+            .then(redis.set.json)
+            .then(_util.auto_fail(done))
+            .catch(_util.ok_error(done))
+    })
     it("normal", function(done) {
         _.promise()
             .then(_util.initialize)
 
             .add({
-                "key": "test/set.json.1",
+                "key": KEY,
                 "json": {
                     "a": [ 1, 2, 3, 4 ],
                 },
@@ -46,6 +72,32 @@ describe("set.json", function() {
             .then(redis.set.json)
 
             .add("json", null)
+            .then(redis.get)
+            .make(sd => {
+                const got = sd.value
+                const want = JSON.stringify({
+                    "a": [ 1, 2, 3, 4 ],
+                })
+                assert.deepEqual(got, want)
+            })
+
+            .end(done)
+    })
+    it("parameterized", function(done) {
+        const KEY = "test/set.json.2"
+
+        _.promise()
+            .then(_util.initialize)
+        
+            .then(redis.set.json.p(KEY, {
+                "a": [ 1, 2, 3, 4 ],
+            }))
+            .make(sd => {
+                assert.ok(!sd.key)
+                assert.ok(!sd.json)
+            })
+
+            .add("key", KEY)
             .then(redis.get)
             .make(sd => {
                 const got = sd.value
